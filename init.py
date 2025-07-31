0bdf2b1bfee1489691fb57e94be0ab0ddfc0a737
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-07-31 22:40:27 +0900

    Update init.py

--- init.py
+++ init.py
@@ -1,1 +1,25 @@
 # TODO: 기본 디렉토리 구조 만들어주는 코드 생성
+
+# essential custom module
+from translator import ts, language
+from module.color import color
+from module.api_request import API_Request
+from module.save_log import save_log
+
+from module.yaml_open import yaml_open
+from module.json_load import json_load
+from module.json_save import json_save
+from module.get_obj import get_obj
+from module.set_obj import set_obj
+from module.cmd_obj_check import cmd_obj_check
+
+from variables.keys import keys
+
+if __name__ == "__main__":
+    obj_origin = json_load()
+    for item in keys:
+        res = get_obj(item)
+        if not res or res is None:
+            set_obj(obj_origin[item], item)
+        else:
+            print(item)
