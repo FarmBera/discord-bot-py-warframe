a8b70f4c4d4c65be3f7a36ddcc1354bd09c97803
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-08-29 21:35:46 +0900

    Updated init script

--- init.py
+++ init.py
@@ -9,46 +9,46 @@ from module.get_obj import get_obj
 from module.set_obj import set_obj
 from var.keys import keys
 
-if __name__ == "__main__":
-    print("API Requesting...")
-    RESPONSE_CODE = API_Request()
-    if RESPONSE_CODE != 200:
-        raise ValueError("Response code is NOT 200!")
-    print(f"Done! Code: {RESPONSE_CODE}")
 
-    dir_base = os.getcwd()
+print("API Requesting...")
+RESPONSE_CODE = API_Request()
+if RESPONSE_CODE != 200:
+    raise ValueError("Response code is NOT 200!")
+print(f"Done! Code: {RESPONSE_CODE}")
 
-    dir_json = [item.replace(".json", "") for item in os.listdir(f"{dir_base}/json")]
+dir_base = os.getcwd()
 
-    prefix: str = "mkdir > "
-    prefix_err: str = "Already exists dir > "
+dir_json = [item.replace(".json", "") for item in os.listdir(f"{dir_base}/json")]
 
-    try:
-        os.mkdir("json")
-        print(f"{prefix}json")
-    except:
-        msg = f"{color['yellow']}{prefix_err}'json'"
-        save_log(cmd="init.py", user="console", msg=msg)
-        print(msg)
+prefix: str = "mkdir > "
+prefix_err: str = "Already exists dir > "
 
-    try:
-        os.mkdir("log")
-        print(f"{prefix}json")
-    except:
-        msg = f"{color['yellow']}{prefix_err}'log'"
-        save_log(cmd="init.py", user="console", msg=msg)
-        print(msg)
+try:
+    os.mkdir("json")
+    print(f"{prefix}json")
+except:
+    msg = f"{color['yellow']}{prefix_err}'json'"
+    save_log(cmd="init.py", user="console", msg=msg)
+    print(msg)
 
-    obj_origin = json_load()
-    for item in keys:
-        if item in dir_json:
-            print(f"{color['green']}File exists > {item}.json")  # VAR
-            continue
+try:
+    os.mkdir("log")
+    print(f"{prefix}json")
+except:
+    msg = f"{color['yellow']}{prefix_err}'log'"
+    save_log(cmd="init.py", user="console", msg=msg)
+    print(msg)
 
-        res = get_obj(item)
-        if not res or res is None:
-            set_obj(obj_origin[item], item)
-        else:
-            print(item)
+obj_origin = json_load()
+for item in keys:
+    if item in dir_json:
+        print(f"{color['green']}File exists > {item}.json")  # VAR
+        continue
 
-    print(color["default"], end="")
+    res = get_obj(item)
+    if not res or res is None:
+        set_obj(obj_origin[item], item)
+    else:
+        print(item)
+
+print(color["default"], end="")
