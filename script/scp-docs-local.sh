cdc93d994365340ea63bf9455a8264e2c6b82b51
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-10-13 01:00:26 +0900

    Fixed Script Typo

--- script/scp-docs-local.sh
+++ script/scp-docs-local.sh
@@ -1,2 +1,2 @@
-scp -r docs $rbpi_local:discord-bot-py-warframe/
-scp -r docs $rbpi_local:discord-bot-py-warframe-kr/
+scp -r docs $rbpi4_local:discord-bot-py-warframe/
+scp -r docs $rbpi4_local:discord-bot-py-warframe-kr/
