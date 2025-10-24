ff4bb1eb3502f533cd521a312881666acc0c5720
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-10-24 16:03:23 +0900

    Fixed `init` script logic

--- src/init.py
+++ src/init.py
@@ -7,25 +7,23 @@ from src.utils.logging_utils import save_log
 from src.utils.file_io import json_load
 from src.utils.data_manager import get_obj
 from src.utils.data_manager import set_obj
-from src.constants.keys import keys
+from src.constants.keys import keys, JSON
 
 
 print("API Requesting...")
-RESPONSE_CODE = API_Request('init')
+RESPONSE_CODE = API_Request("init")
 if RESPONSE_CODE != 200:
     raise ValueError("Response code is NOT 200! Aborted!")
 print(f"Done! Code: {RESPONSE_CODE}")
 
 dir_base = os.getcwd()
 
-dir_json = [item.replace(".json", "") for item in os.listdir(f"{dir_base}/json")]
-
 prefix: str = "mkdir > "
 prefix_err: str = "Already exists dir > "
 
 try:
-    os.mkdir("json")
-    print(f"{prefix}json")
+    os.mkdir(f"{dir_base}/{JSON}")
+    print(f"{prefix}{JSON}")
 except:
     msg = f"{prefix_err}'json'"
     save_log(cmd="init.py", user="console", msg=msg)
@@ -33,11 +31,14 @@ except:
 
 try:
     os.mkdir("log")
-    print(f"{prefix}json")
+    print(f"{prefix}log")
 except:
     msg = f"{prefix_err}'log'"
     save_log(cmd="init.py", user="console", msg=msg)
     print(C.yellow, msg)
+
+
+dir_json = [item.replace(".json", "") for item in os.listdir(f"{dir_base}/{JSON}")]
 
 obj_origin = json_load()
 for item in keys:
@@ -46,7 +47,7 @@ for item in keys:
         # continue
 
     # res = get_obj(item)
-    # if not res or res is None:
+    # if not res:
     set_obj(obj_origin[item], item)
     # else:
     print(f"{C.cyan}{item}")
