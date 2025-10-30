3d48e4a2e0f385d623545538170570853a763c6b
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-10-30 19:50:16 +0900

    Update backup script

--- script/scp-log-file-local.sh
+++ script/scp-log-file-local.sh
@@ -1,2 +1,4 @@
-scp $rbpi4_local:discord-bot-py-warframe/log/log.csv log-en.csv
-scp $rbpi4_local:discord-bot-py-warframe-kr/log/log.csv log-kr.csv
+CURRENT_DATETIME=$(date "+%y%m%d-%H%M")
+
+scp $rbpi4_local:discord-bot-py-warframe/log/log.csv log/log-en-$CURRENT_DATETIME.csv
+scp $rbpi4_local:discord-bot-py-warframe-kr/log/log.csv log/log-kr-$CURRENT_DATETIME.csv
