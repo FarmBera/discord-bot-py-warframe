96098419ed8524a8b47994decc5856a8502b1f21
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-10-31 12:55:54 +0900

    Updated maintenance msg

--- src/commands/cmd_maintenance.py
+++ src/commands/cmd_maintenance.py
@@ -17,10 +17,10 @@ async def cmd_helper_maintenance(interact: discord.Interaction) -> None:
 
     txt = f"""# 서버 점검 중
 
-지금은 서버 점검 및 패치 작업으로 인하여 명령어를 사용할 수 없습니다.
+지금은 **서버 점검 및 패치 작업**으로 인하여 **봇을 사용할 수 없습니다.**
 이용에 불편을 드려 죄송합니다.
 
-> 종료까지 **{time_format(time_left)}** 남았습니다.
+> 종료까지 약 **{time_format(time_left)}** 남았습니다.
 > 예상 완료 시간: {dt.datetime.strftime(time_target,"%Y-%m-%d %H:%M")}
 
 패치 작업은 조기 종료 될 수 있으며, 또한 지연될 수 있음을 알립니다.
