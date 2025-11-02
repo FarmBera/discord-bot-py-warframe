236c1ef54647b83a06a1b52bd2e3bbb5feae306a
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-11-02 17:53:08 +0900

    Updated Emoji

--- src/utils/emoji.py
+++ src/utils/emoji.py
@@ -3,76 +3,78 @@ def get_emoji(item: str):
 
     # 7; general items
     if "riven" in item:
-        return "<:rivenmod:1407864916492288070>"
+        return "<:rivenmod:1434448154371031152>"
     elif "kuva" in item:
-        return "<:kuva:1407864912839049269>"
+        return "<:kuva:1434448114076356650>"
     elif "endo" in item:
-        return "<:endo:1407864907235463319>"
+        return "<:endo:1434448109097844766>"
     elif "arcane enhancements" in item:
-        return "<:ArcaneEnhance:1407880465146314785>"
+        return "<:ArcaneEnhance:1434447901832253542>"
     elif "reactor" in item:
-        return "<:OrokinReactor:1407882689331335188>"
+        return "<:OrokinReactor:1434447908450734180>"
     elif "catalyst" in item:
-        return "<:OrokinCatalyst:1407882687032852551>"
+        return "<:OrokinCatalyst:1434447906823340032>"
     elif "ducat" in item:
-        return "<:ducats:1408813306415808676>"
+        return "<:ducats:1434448107411607634>"
     elif "credit" in item:
-        return "<:credits:1408813303798563077>"
+        return "<:credits:1434448106111631471>"
     # 2; forma
     elif "forma" in item:
-        return "<:forma:1407864910997622916>"
+        return "<:forma:1434448112113549332>"
     elif "aura forma" in item:
-        return "<:auraforma:1407864904404172850>"
+        return "<:auraforma:1434448094316990585>"
+    elif "umbra" in item:
+        return "<:umbraforma:1434448156363587714>"
 
     # 5; adapter
     elif "exilus weapon" in item:
-        return "<:ExilusWeaponAdapter:1407881645784305759>"
+        return "<:ExilusWeaponAdapter:1434447905304871004>"
     elif "exilus warframe" in item:
-        return "<:ExilusWarframeAdapter:1407881644022435961>"
+        return "<:ExilusWarframeAdapter:1434447903752982608>"
     elif "primary arcane" in item:
-        return "<:PrimaryArcaneAdapter:1407881651727368302>"
+        return "<:PrimaryArcaneAdapter:1434447878675234916>"
     elif "secondary arcane" in item:
-        return "<:SecondaryArcaneAdapter:1407881654424567869>"
+        return "<:SecondaryArcaneAdapter:1434447880898351155>"
     elif "melee arcane" in item:
-        return "<:MeleeArcaneAdapter:1407881648825045135>"
+        return "<:MeleeArcaneAdapter:1434447877018619944>"
 
     # 6; shard
     elif "crimson" in item:
-        return "<:CrimsonArchonShard:1407864644923691028>"
+        return "<:CrimsonArchonShard:1434447996145242122>"
     elif "azure" in item:
-        return "<:AzureArchonShard:1407864641714913384>"
+        return "<:AzureArchonShard:1434447994165661788>"
     elif "amber" in item:
-        return "<:AmberArchonShard:1407864636698529924>"
+        return "<:AmberArchonShard:1434447992231956523>"
     elif "emerald" in item:
-        return "<:EmeraldArchonShard:1407864648409026580>"
+        return "<:EmeraldArchonShard:1434448003959095328>"
     elif "topaz" in item:
-        return "<:TopazArchonShard:1407864650510372918>"
+        return "<:TopazArchonShard:1434448015132856430>"
     elif "violet" in item:
-        return "<:VioletArchonShard:1407864652175642665>"
+        return "<:VioletArchonShard:1434448017620209747>"
 
     # 5; void relics
     elif item in ["axi", "액시"]:
-        return "<:AxiRelic:1408056877992116355>"
+        return "<:AxiRelic:1434447931007696926>"
     elif item in ["lith", "리스"]:
-        return "<:LithRelic:1408056882051944489>"
-    elif item in ["meso",'메소']:
-        return "<:MesoRelic:1408056873747484713>"
-    elif item in ["neo",'네오']:
-        return "<:NeoRelic:1408056875735584818>"
-    elif item in ["requiem",'레퀴엠']:
-        return "<:RequiemRelic:1408056880206577705>"
+        return "<:LithRelic:1434447932303872102>"
+    elif item in ["meso", "메소"]:
+        return "<:MesoRelic:1434447933708959796>"
+    elif item in ["neo", "네오"]:
+        return "<:NeoRelic:1434447940130177114>"
+    elif item in ["requiem", "레퀴엠"]:
+        return "<:RequiemRelic:1434447942516740130>"
 
     # 5; 3 day boosters
     elif "affinity" in item:
-        return "<:3DayAffinity:1407881490381013034>"
+        return "<:3DayAffinity:1434447845548753020>"
     elif "mod" in item:
-        return "<:3DayModDropChance:1407881495023980704>"
+        return "<:3DayModDropChance:1434447848933687329>"
     elif "credit" in item:
-        return "<:3DayCredit:1407881492553531432>"
+        return "<:3DayCredit:1434447847054376960>"
     elif "resource drop" in item:
-        return "<:3DayResourceDropChance:1407881500409466942>"
+        return "<:3DayResourceDropChance:1434447851923963945>"
     elif "resource" in item:
-        return "<:3DayResource:1407881497976901773>"
+        return "<:3DayResourceDropChance:1434447851923963945>"
 
     else:
         return ""
