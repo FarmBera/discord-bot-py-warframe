8931b353abbd6f9867cbeed217ebae7d061adc99
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-11-05 17:23:22 +0900

    Improved log backup script

--- script/scp-log-file-local.sh
+++ script/scp-log-file-local.sh
@@ -1,4 +1,6 @@
 CURRENT_DATETIME=$(date "+%y%m%d-%H%M")
 
-scp $rbpi4_local:discord-bot-py-warframe/log/log.csv log/log-en-$CURRENT_DATETIME.csv
-scp $rbpi4_local:discord-bot-py-warframe-kr/log/log.csv log/log-kr-$CURRENT_DATETIME.csv
+mkdir log/remote/$CURRENT_DATETIME
+
+scp $rbpi4_local:wf/log/log.csv log/remote/$CURRENT_DATETIME/log-en.csv
+scp $rbpi4_local:wfk/log/log.csv log/remote/$CURRENT_DATETIME/log-kr.csv
--- script/scp-log-file-remote.sh
+++ script/scp-log-file-remote.sh
@@ -1,4 +1,6 @@
 CURRENT_DATETIME=$(date "+%y%m%d-%H%M")
 
-scp $rbpi4_remote:discord-bot-py-warframe/log/log.csv log/log-en-$CURRENT_DATETIME.csv
-scp $rbpi4_remote:discord-bot-py-warframe-kr/log/log.csv log/log-kr-$CURRENT_DATETIME.csv
+mkdir log/remote/$CURRENT_DATETIME
+
+scp $rbpi4_remote:wf/log/log.csv log/remote/$CURRENT_DATETIME/log-en.csv
+scp $rbpi4_remote:wfk/log/log.csv log/remote/$CURRENT_DATETIME/log-kr.csv
