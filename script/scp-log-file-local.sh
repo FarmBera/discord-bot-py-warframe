be9da53dfcd4db7ee74c1a2a73d94d01360a8028
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-11-07 09:47:02 +0900

    Update script: log backup

--- script/scp-log-file-local.sh
+++ script/scp-log-file-local.sh
@@ -2,5 +2,5 @@ CURRENT_DATETIME=$(date "+%y%m%d-%H%M")
 
 mkdir log/remote/$CURRENT_DATETIME
 
-scp $rbpi4_local:wf/log/log.csv log/remote/$CURRENT_DATETIME/log-en.csv
-scp $rbpi4_local:wfk/log/log.csv log/remote/$CURRENT_DATETIME/log-kr.csv
+scp $rbpi4_local:wf/log/log.csv log/remote/$CURRENT_DATETIME/log-en-$CURRENT_DATETIME.csv
+scp $rbpi4_local:wfk/log/log.csv log/remote/$CURRENT_DATETIME/log-kr-$CURRENT_DATETIME.csv
--- script/scp-log-file-remote.sh
+++ script/scp-log-file-remote.sh
@@ -2,5 +2,5 @@ CURRENT_DATETIME=$(date "+%y%m%d-%H%M")
 
 mkdir log/remote/$CURRENT_DATETIME
 
-scp $rbpi4_remote:wf/log/log.csv log/remote/$CURRENT_DATETIME/log-en.csv
-scp $rbpi4_remote:wfk/log/log.csv log/remote/$CURRENT_DATETIME/log-kr.csv
+scp $rbpi4_remote:wf/log/log.csv log/remote/$CURRENT_DATETIME/log-en-$CURRENT_DATETIME.csv
+scp $rbpi4_remote:wfk/log/log.csv log/remote/$CURRENT_DATETIME/log-kr-$CURRENT_DATETIME.csv
