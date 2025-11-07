64d3f728ae28abc64cd37f22fc7fbe4b01af46c6
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-11-07 10:53:00 +0900

    Update scp-db-file.sh

--- script/scp-db-file.sh
+++ script/scp-db-file.sh
@@ -1,4 +1,6 @@
+TARGET=$rbpi4_remote
+
 CURRENT_DATETIME=$(date "+%y%m%d-%H%M%S")
 
 echo $CURRENT_DATETIME
-scp -r $rbpi4_local:discord-bot-py-warframe-kr/db backup/db/db-$CURRENT_DATETIME
+scp -r $TARGET:wfk/db/party.db backup/db/party-$CURRENT_DATETIME.db
