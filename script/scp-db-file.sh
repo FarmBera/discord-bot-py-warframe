cf48f29f9d7b56bb39801e9d6e7043a960b95fc0
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-11-20 16:00:31 +0900

    Update db backup script

--- script/scp-db-file.sh
+++ script/scp-db-file.sh
@@ -1,6 +1,7 @@
 TARGET=$rbpi4_remote
 
+DB_NAME=party
 CURRENT_DATETIME=$(date "+%y%m%d-%H%M%S")
 
 echo $CURRENT_DATETIME
-scp -r $TARGET:wfk/db/party.db backup/db/party-$CURRENT_DATETIME.db
+scp -r $TARGET:wfk/db/$DB_NAME.db backup/db/$DB_NAME-$CURRENT_DATETIME.db
