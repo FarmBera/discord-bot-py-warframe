02ea51d070ad834d9e826aaba77dd657227f067b
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-12-01 09:58:14 +0900

    Fix duviri notification (ft. condition)

--- src/client/bot_main.py
+++ src/client/bot_main.py
@@ -356,7 +356,7 @@ class DiscordBot(discord.Client):
 
             elif special_logic == "handle_duviri_rotation-1":  # circuit-warframe
                 is_new = set(duv_warframe["Choices"]) != set(obj_new[0]["Choices"])
-                if not is_new:
+                if is_new:
                     continue
 
                 try:
@@ -380,7 +380,7 @@ class DiscordBot(discord.Client):
 
             elif special_logic == "handle_duviri_rotation-2":  # circuit-incarnon
                 is_new = set(duv_incarnon["Choices"]) != set(obj_new[1]["Choices"])
-                if not is_new:
+                if is_new:
                     continue
 
                 try:
