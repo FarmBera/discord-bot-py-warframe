03e4475790c5dbe24881ea0d582e48d00e6108c1
FarmBera <101335222+FarmBera@users.noreply.github.com>
2025-12-11 23:44:56 +0900

    Remote script dir

--- script/scp-log-file-local.sh
+++
@@ -1,6 +0,0 @@
-CURRENT_DATETIME=$(date "+%y%m%d-%H%M")
-
-mkdir log/remote/$CURRENT_DATETIME
-
-scp $rbpi4_local:wf/log/log.csv log/remote/$CURRENT_DATETIME/log-en-$CURRENT_DATETIME.csv
-scp $rbpi4_local:wfk/log/log.csv log/remote/$CURRENT_DATETIME/log-kr-$CURRENT_DATETIME.csv
