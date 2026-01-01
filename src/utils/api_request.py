82348920b9938c9dddb5fac3e31492866594e295
FarmBera <101335222+FarmBera@users.noreply.github.com>
2026-01-01 11:00:21 +0900

    Improve api request log

--- src/utils/api_request.py
+++ src/utils/api_request.py
@@ -4,10 +4,12 @@ import json
 
 from src.translator import language as lang
 from config.TOKEN import base_url_warframe, base_url_market, WF_JSON_PATH
+from config.config import LOG_TYPE
 from src.constants.color import C
 from src.constants.keys import MSG_BOT
 from src.utils.times import timeNowDT
 from src.utils.logging_utils import save_log
+from src.utils.return_err import return_traceback
 
 params_market: dict = {"Language": lang, "Platform": "pc"}
 
@@ -32,16 +34,15 @@ async def API_Request(pool, res_source: str = "Unknown Source"):
         response = requests.get(base_url_warframe, timeout=60)
     except Exception as e:
         elapsed_time = timeNowDT() - start_time
-        msg = f"[err] API request failed!"
-        obj = f"{elapsed_time}/{e}"
-        print(timeNowDT(), C.red, msg, obj, elapsed_time, C.default)
+        msg = f"[err] API request failed!: {elapsed_time}"
+        print(timeNowDT(), C.red, msg, C.default)
         await save_log(
             pool=pool,
-            type="err",
-            cmd="send_request()",
+            type=LOG_TYPE.err,
+            cmd="API_Request()",
             user=MSG_BOT,
             msg=msg,
-            obj=obj,
+            obj=return_traceback(),
         )
         return None
 
@@ -50,33 +51,30 @@ async def API_Request(pool, res_source: str = "Unknown Source"):
     if res_code != 200:
         elapsed_time = timeNowDT() - start_time
 
-        msg = f"[warn] response code is not 200"
-        obj = f"{res_code}/{elapsed_time}"
+        msg = f"[warn] response code is not 200: {res_code}/{elapsed_time}"
         await save_log(
             pool=pool,
-            type="err",
-            cmd="API_REQUEST()",
+            type=LOG_TYPE.err,
+            cmd="API_Request()",
             user=MSG_BOT,
             msg=msg,
-            obj=obj,
         )
-        print(timeNowDT(), C.red, msg, res_code, elapsed_time, C.default)
+        print(timeNowDT(), C.red, msg, C.default)
         return response
 
     # check response (is not empty or err value)
     if response is None:
         elapsed_time = timeNowDT() - start_time
 
-        msg = f"[err] response is Empty!"
-        obj = f"{res_code}/{elapsed_time}/{response}"
-        print(timeNowDT(), C.red, msg, res_code, elapsed_time, C.default)
+        msg = f"[err] response is Empty!: {res_code}/{elapsed_time}"
+        print(timeNowDT(), C.red, msg, C.default)
         await save_log(
             pool=pool,
-            type="api",
-            cmd="API_REQUEST()",
+            type=LOG_TYPE.err,
+            cmd="API_Request()",
             user=MSG_BOT,
             msg=msg,
-            obj=obj,
+            obj=response,
         )
         return None
 
@@ -86,30 +84,26 @@ async def API_Request(pool, res_source: str = "Unknown Source"):
             json.dump(response.json(), json_file, ensure_ascii=False, indent=2)
     except Exception as e:
         elapsed_time = timeNowDT() - start_time
-        msg = f"[err] Error on saving file!"
-        obj = f"{elapsed_time}/{e}"
-        print(timeNowDT(), C.red, msg, elapsed_time, C.default)
+        msg = f"[err] Error on saving file: {elapsed_time}/{e}"
+        print(timeNowDT(), C.red, msg, C.default)
         await save_log(
             pool=pool,
-            type="err",
-            cmd="API_REQUEST()",
+            type=LOG_TYPE.err,
+            cmd="API_Request()",
             user=MSG_BOT,
             msg=msg,
-            obj=obj,
+            obj=return_traceback(),
         )
 
-    elapsed_time = timeNowDT() - start_time
-    msg = f"[info] API request successful. {res_source}"
+    msg = f"{res_code}//{res_source}//{response.elapsed}"
     # print(C.red, msg, C.default, sep="")
     await save_log(
         pool=pool,
-        type="api",
-        cmd="API_REQUEST()",
+        type=LOG_TYPE.api,
+        cmd="API_Request()",
         user=MSG_BOT,
         msg=msg,
-        obj=f"{res_code}/{elapsed_time}",
     )
-
     return response
 
 
@@ -136,15 +130,15 @@ async def API_MarketSearch(pool, item_name: str):
             timeout=60,
         )
     except Exception as e:
-        msg = f"[err] API request failed! (from API_MarketSearch)"
-        print(timeNowDT(), C.yellow, msg, C.red, e, C.default)
+        msg = f"[err] API request failed!: {e}"
+        print(timeNowDT(), C.red, msg, C.default)
         await save_log(
             pool=pool,
-            type="api",
+            type=LOG_TYPE.err,
             cmd="API_MarketSearch()",
             user=MSG_BOT,
             msg=msg,
-            obj=f"{response.res_code}/{e}",
+            obj=return_traceback(),
         )
         return None
 
@@ -158,7 +152,23 @@ async def API_MarketSearch(pool, item_name: str):
     # check response (is not empty)
     if response is None:
         msg = f"[err] response is Empty!"
+        await save_log(
+            pool=pool,
+            type=LOG_TYPE.err,
+            cmd="API_MarketSearch()",
+            user=MSG_BOT,
+            msg=msg,
+        )
         print(C.red, msg, C.default)
         return response
+
+    msg = f"{response.status_code}//{item_name}//{response.elapsed}"
+    await save_log(
+        pool=pool,
+        type=LOG_TYPE.api,
+        cmd="API_MarketSearch()",
+        user=MSG_BOT,
+        msg=msg,
+    )
 
     return response
