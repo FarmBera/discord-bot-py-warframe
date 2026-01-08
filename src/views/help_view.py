cf01b9a95e0798013d9d221c04281a8ece1358f7
FarmBera <101335222+FarmBera@users.noreply.github.com>
2026-01-08 13:59:25 +0900

    Add help cmd: support server btn

--- src/views/help_view.py
+++ src/views/help_view.py
@@ -1,7 +1,7 @@
 from discord.ui import View, Button
 
 from src.translator import ts
-from config.TOKEN import HOMEPAGE, DONATION
+from config.TOKEN import HOMEPAGE, DONATION, SUPPORT_SERVER
 
 pf: str = "view.support-"
 
@@ -10,9 +10,12 @@ class SupportView(View):
     def __init__(self):
         super().__init__(timeout=None)
 
-        self.add_item(Button(label=ts.get(f"{pf}help"), url=f"{HOMEPAGE}"))
-        self.add_item(Button(label=ts.get(f"{pf}tos"), url=f"{HOMEPAGE}?id=tos"))
         self.add_item(
-            Button(label=ts.get(f"{pf}privacy"), url=f"{HOMEPAGE}/?id=privacy")
+            Button(label=ts.get(f"{pf}support"), url=f"{SUPPORT_SERVER}", row=1)
         )
-        self.add_item(Button(label=ts.get(f"{pf}donate"), url=DONATION, row=1))
+        self.add_item(Button(label=ts.get(f"{pf}help"), url=f"{HOMEPAGE}", row=2))
+        self.add_item(Button(label=ts.get(f"{pf}tos"), url=f"{HOMEPAGE}?id=tos", row=2))
+        self.add_item(
+            Button(label=ts.get(f"{pf}privacy"), url=f"{HOMEPAGE}/?id=privacy", row=2)
+        )
+        self.add_item(Button(label=ts.get(f"{pf}donate"), url=DONATION, row=3))
